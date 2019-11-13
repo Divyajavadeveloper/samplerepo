fcd45fb9c0cd2347351ba0e4191df7da451a43c4
Vijay Thonupunoori <Vijay Thonupunoori@DESKTOP-6GK6CDA.home>
2019-11-13 07:24:48 -0600

    Ititial Push for MsService

---
+++ MsServiceBasics/src/main/java/com/example/demo/MsServiceBasicsApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsServiceBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsServiceBasicsApplication.class, args);
+		
+		
+			}
+
+
+}
---
+++ MsServiceBasics/src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Vijay Thonupunoori
+ *
+ */
+@RestController
+public class LoginController {
+	
+@RequestMapping("/login")	
+public String login()
+{
+	return "Hello World";
+}
+}
