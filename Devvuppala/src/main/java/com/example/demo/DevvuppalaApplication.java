a74e19360a3ae01dde8ba93cf7f56a4d5d8e9ab5
Vijay Thonupunoori <Vijay Thonupunoori@DESKTOP-6GK6CDA.home>
2019-11-13 07:19:25 -0600

    Intial push

---
+++ Devvuppala/src/main/java/com/example/demo/DevvuppalaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevvuppalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevvuppalaApplication.class, args);
+	}
+
+}
